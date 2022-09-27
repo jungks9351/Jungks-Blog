33f3f24d4634b8179b9d0fb678592087ff2f9da2
kimjungjin <jungks9351@gmail.com>
2022-09-27 23:58:31 +0900

    프로젝트 생성

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Jungks Blog</title>
+        <meta name="description" content="Create Next.js Blog" />
+      </Head>
+      <div>Hello Next.js!</div>
+    </>
+  )
+}
+
+export default Home
