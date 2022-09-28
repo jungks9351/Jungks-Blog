5fd34c311fa33afd435393de7de1da3046592633
kimjungjin <jungks9351@gmail.com>
2022-09-29 01:30:09 +0900

    refactor : IntroSection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,7 @@ import Head from 'next/head'
 import { compareDesc } from 'date-fns'
 import { allPosts, type Post } from 'contentlayer/generated'
 import PostCard from 'src/components/PostCard'
-import { useRef } from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import IntroduceSection from 'src/components/home/IntroduceSection'
+import IntroSection from 'src/components/home/IntroSection'
 
 export const getStaticProps = async () => {
   const posts: Post[] = allPosts.sort((a, b) => {
@@ -20,12 +17,7 @@ const Home = ({ posts }: { posts: Post[] }) => {
       <Head>
         <title>JungJin Blog</title>
       </Head>
-      {/* <section className="py-10">
-        <h2 className="leading-tight text-3xl md:text-4xl text-black dark:text-white">
-          words
-        </h2>
-      </section> */}
-      <IntroduceSection />
+      <IntroSection />
       {posts.map((post, idx) => (
         <PostCard key={idx} post={post} />
       ))}
