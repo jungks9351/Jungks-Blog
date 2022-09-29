3e6750e192b0f1baa1906499c0e4326ee4d74c0c
kimjungjin <jungks9351@gmail.com>
2022-09-30 01:07:27 +0900

    style : Footer 하단 고정 스타일 삭제

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer className="p-5 absolute bottom-0 left-0 right-0">footer</footer>
-  )
+  return <footer className="p-5">footer</footer>
 }
 
 export default Footer
