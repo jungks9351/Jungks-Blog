85b26e58ff0f376997fd2f018e90f6e09d5141be
kimjungjin <jungks9351@gmail.com>
2022-10-01 01:30:07 +0900

    feat : 동적인 햄버거 메뉴 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,6 +6,9 @@ import { useTheme } from 'next-themes'
 import SunIcon from 'public/svgs/sun_icon.svg'
 import MoonIcon from 'public/svgs/moon_icon.svg'
 
+import navLinks from 'src/data/navlinks'
+import MenuButton from './MenuButton'
+
 const Header = () => {
   const { theme, setTheme } = useTheme()
   const [loaded, setLoaded] = useState(false)
@@ -18,28 +21,31 @@ const Header = () => {
   }
 
   return (
-    <header className="py-2 text-primary mx-auto flex max-w-8xl items-center justify-between">
-      <Link href={'/'}>
-        <h1 className="cursor-pointer">
-          <Image
-            src="/images/blog_logo.png"
-            alt="blog logo"
-            layout="fixed"
-            width={50}
-            height={50}
-            objectFit="cover"
-          />
-        </h1>
-      </Link>
-      <div className="">
-        <button
-          onClick={toggleTheme}
-          title="darkmode toggle button"
-          className="w-10 h-10 px-2 rounded-full border-2 border-main-purple dark:border-main-purple flex items-center justify-center"
-        >
-          {loaded && theme === 'light' ? <SunIcon /> : <MoonIcon />}
-        </button>
-      </div>
+    <header className="absolute top-0 z-[100] w-full px-8 bg-white dark:bg-gray-900">
+      <nav className="flex items-center justify-between">
+        <Link href={'/'}>
+          <h1 className="cursor-pointer">
+            <Image
+              src="/images/blog_logo.png"
+              alt="blog logo"
+              layout="fixed"
+              width={50}
+              height={50}
+              objectFit="cover"
+            />
+          </h1>
+        </Link>
+        <div className="flex gap-5">
+          <MenuButton />
+          <button
+            onClick={toggleTheme}
+            title="darkmode toggle button"
+            className="w-10 h-10 px-2 rounded-full border-2 border-black dark:border-white flex items-center justify-center"
+          >
+            {loaded && theme === 'light' ? <SunIcon /> : <MoonIcon />}
+          </button>
+        </div>
+      </nav>
     </header>
   )
 }
---
+++ src/components/common/MenuButton.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import MenuIcon from 'public/svgs/menu_icon.svg'
+
+const MenuButton = () => {
+  const [active, setActive] = useState(false)
+
+  const toggleAnimation = () => {
+    setActive(!active)
+  }
+
+  return (
+    <button
+      onClick={toggleAnimation}
+      className="rounded-full border-2 border-black dark:border-white"
+      title="menu button"
+    >
+      <MenuIcon
+        className={
+          active ? 'menu-trigger active text-4xl' : 'menu-trigger text-4xl'
+        }
+      />
+    </button>
+  )
+}
+
+export default MenuButton
