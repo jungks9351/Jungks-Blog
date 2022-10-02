104b558a02e1e39453fdb1d6616ba09539de8372
kimjungjin <jungks9351@gmail.com>
2022-10-03 05:00:48 +0900

    feat : 푸터 구현

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import GitLogo from 'public/svgs/github.svg'
 
 const Footer = () => {
-  return <footer className="p-5">footer</footer>
+  return (
+    <footer className="p-5 flex flex-col items-center justify-center gap-4">
+      <a href="https://github.com/jungks9351" target="_blank" rel="noreferrer">
+        <GitLogo />
+      </a>
+      <p className="text-center">Copyright ⓒ2022 JungJinKim.</p>
+    </footer>
+  )
 }
 
 export default Footer
